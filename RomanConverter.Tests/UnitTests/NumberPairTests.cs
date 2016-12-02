284c0e86548decd0fcb58c5948af2789f3e79837
Craig Banach <Craig Banach>
2016-12-02 00:09:49 +0000

    Added new failing unit tests.

--- RomanConverter.Tests/UnitTests/NumberPairTests.cs
+++ RomanConverter.Tests/UnitTests/NumberPairTests.cs
@@ -1,33 +1,46 @@
+using Microsoft.Extensions.Configuration;
 using RomanConverter.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using System.Diagnostics;
 
 namespace RomanConverter.Tests.UnitTests
 {
   public class NumberPairTests
   {
-    //Arrange
-    private NumberPair testNumberPair;
+    private NumberPair[] TestPairs = new NumberPair[7];
+    public IConfigurationRoot Configuration;
 
     public NumberPairTests() {
 
-      testNumberPair = new NumberPair()
-      {
-        Base10 = 1,
-      };
+      TestPairs[0] = new NumberPair() { Base10 = 1, Numeral = "I" };
+      TestPairs[1] = new NumberPair() { Base10 = 5, Numeral = "V" };
+      TestPairs[2] = new NumberPair() { Base10 = 10, Numeral = "X" };
+      TestPairs[3] = new NumberPair() { Base10 = 50, Numeral = "L" };
+      TestPairs[4] = new NumberPair() { Base10 = 100, Numeral = "C" };
+      TestPairs[5] = new NumberPair() { Base10 = 500, Numeral = "D" };
+      TestPairs[6] = new NumberPair() { Base10 = 100, Numeral = "M" };
     }
-
+    
     [Fact]
     public void ConvertsBase10ToNumerals()
     {
-      //Act
-      testNumberPair.Convert();
 
-      //Assert
-      Assert.Equal( testNumberPair, new NumberPair{ Base10 = 1, Numeral = "I" }, new NumberPairComparer());
+      //Arrange
+      for (int i = 0 ; i < 7 ; i++ )
+      {
+        NumberPair testNumberPair = new NumberPair() { Base10 = TestPairs[i].Base10 };
+
+        //Act
+        testNumberPair.Convert();
+
+        //Assert
+        Assert.Equal(testNumberPair, TestPairs[i], new NumberPairComparer());
+      }
     }
   }
 }
